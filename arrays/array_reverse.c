6c2afd096aed1d44e464b16b89885e25012f3b5b
sai-embedded <gsaishruthi.13@gmail.com>
2026-06-16 22:16:44 +0000

    refactored reverse array

--- arrays/array_reverse.c
+++ arrays/array_reverse.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-void reverse_array_use_pointers(int *arr, int size)
+void reverse_array_use_pointers(int *arr, int n)
 {
-    if (arr == NULL || size <= 1) // need min 2 elements to reverse
+    if (arr == NULL || n <= 1) // need min 2 elements to reverse
     {
         return;
     }
 
     int *start = arr;
-    int *end = arr + size - 1;
+    int *end = arr + n - 1;
+    // (int*)arr + (n-1)*sizeof(int)
+    // “Pointer arithmetic is scaled by the size of the data type, 
+    // so arr + i moves &arr[0] + i * sizeof(type) bytes.”
 
     // Space complexity O(1), time complexity O(n)
     while (start < end)
@@ -16,6 +19,7 @@ void reverse_array_use_pointers(int *arr, int size)
         int temp = *start;
         *start = *end;
         *end = temp;
+        
         start++;
         end--;
     }
@@ -39,6 +43,25 @@ void reverse_array(int *arr, int size)
         arr[start] = arr[end];
         arr[end] = temp;
         start++;
+        end--;
+    }
+}
+
+void reverse_string(char *str)
+{
+    if(str == NULL)
+        return;
+
+    int st = 0;
+    int end = strlen(str) - 1; // strlen excludes NULL char so no need to rewrite NULL after reverse string
+
+    while(st < end)
+    {
+        char temp = str[st];
+        str[st] = str[end];
+        str[end] = temp;
+
+        st++;
         end--;
     }
 }
