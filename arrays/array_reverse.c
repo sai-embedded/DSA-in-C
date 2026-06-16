70b9dd8719e936e08d99ceae5da6314233ffdec6
sai-embedded <gsaishruthi.13@gmail.com>
2026-06-16 22:00:45 +0000

    Cleaned up the code

--- arrays/array_reverse.c
+++ arrays/array_reverse.c
@@ -1,47 +1,27 @@
 #include <stdio.h>
 
-// int main()
-// {
-//     // Given an array, reverse it in-place. Output arr[] = {4,3,2,1}
-//     int arr[] = {1,2,3,4,5,6};
+void reverse_array_use_pointers(int *arr, int size)
+{
+    if (arr == NULL || size <= 1) // need min 2 elements to reverse
+    {
+        return;
+    }
 
-//     int n = sizeof(arr)/sizeof(arr[0]);
+    int *start = arr;
+    int *end = arr + size - 1;
 
-//     if(arr == NULL || n <= 1)
-//         return; // Need minimum 2 elements to reverse
+    // Space complexity O(1), time complexity O(n)
+    while (start < end)
+    {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
 
-//     int temp;
-
-//     for(int i=0; i<n; i++) // time complexity : O(n)
-//     {
-//         printf("original arr arr[%d] = %d\n", i, arr[i]);
-//     }
-
-//     /*
-//     “I’ll use a two-pointer style approach conceptually.
-//     One side starts from the beginning and the other from the end.
-//     During each iteration I swap symmetric elements and move inward.
-//     Since each swap places two elements into their final positions,
-//     we only need to iterate until the midpoint of the array.”
-//     */
-//     for(int i=0; i<n/2; i++) // time complexity : O(n/2)
-//     {
-//         temp = arr[i];
-//         arr[i] = arr[n-1-i];
-//         arr[n-1-i] = temp;
-//     }
-
-//     for(int i=0; i<n; i++) // time complexity O(n)
-//     {
-//         printf("rev arr arr[%d] = %d\n", i, arr[i]);
-//     }
-
-//     // total time complexity = O(0.5n) ~= O(n)
-//     // space complexity O(1) no extra memory needed based on growing input size
-
-//     return 0;
-// }
-
+// indexing approach
 void reverse_array(int *arr, int size)
 {
     if (arr == NULL || size <= 1) // need min 2 elements to reverse
